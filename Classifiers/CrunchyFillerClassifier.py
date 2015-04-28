0fa9bd104b76e393b61a476e4cfc947c29678594
nadyac <nadyacpena@gmail.com>
2015-04-28 18:54:21 -0400

    Fixed bug that was causing the misfiring of DivisionByZeroException in shows with 0 filler comments or words (see issue #1 for details)

--- Classifiers/CrunchyFillerClassifier.py
+++ Classifiers/CrunchyFillerClassifier.py
@@ -58,46 +58,49 @@ def classifyFillerOrCanon(probabilityOfFiller, document):
 	commentsWithFillerWords = 0 #this is also a feature
 
 	# read all the comments in each document
-	for words in document:
+	for comments in document:
 
 		# if this episode contains comments with filler words, it could be 
 		# a filler, so add increment the comment count
-		if commentsWithFillerWords >= 1:
-			commentsCount += 1
+		#if commentsWithFillerWords >= 1:
+		commentsCount += 1
 
 		incrementFlag = False # prevents commentsWithFillerWords from being incremented more than once per comment
 
 		# clean the words in the csvfile to remove unncessary words and
 		# characters/punctuations
-		word = words.lower()
-		word = ReplaceCharsInString.cleanString(word)
-		word = word.replace("-", "")
+		comment = comments.lower()
+		comment = comment.replace("-", "")
+		
+		#loop through each word in each comment
+		for word in comment.split():
+			word = ReplaceCharsInString.cleanString(word)
 
-		# filter out words that don't add to the meaning of the comment
-		if word != 'i' and word != 'this' and word != 'that' and \
-		word != 'they' and word != 'we' and word != 'you' and \
-		word != 'me' and word != 'he' and word != 'she' and word != 'it' \
-		and word != 'them' and word != 'to' and word != 'for' and \
-		word != 'with' and word != 'have' and word != 'be' and \
-		word != 'been' and word != 'a' and word != 'or' and word != 'if'\
-		and word != 'are' and word != 'when' and word != 'the' and \
-		word != 'and' and word != 'in' and word != 'my' and word.find('@') == -1\
-		and word != 'at' and word != 'on' and word != 'there' and \
-		word != 'their' and word != 'theyre' and word != 'how' and word != 'as' \
-		and word != 'if' and word != 'was' and word != 'has' and word != 'had' \
-		and word != 'is' and word != " " and word.find('=') == -1 and word.find('_') == -1 \
-		and word.find('<') == -1 and word.find('>') == -1:
+			# filter out words that don't add to the meaning of the comment
+			if word != 'i' and word != 'this' and word != 'that' and \
+			word != 'they' and word != 'we' and word != 'you' and \
+			word != 'me' and word != 'he' and word != 'she' and word != 'it' \
+			and word != 'them' and word != 'to' and word != 'for' and \
+			word != 'with' and word != 'have' and word != 'be' and \
+			word != 'been' and word != 'a' and word != 'or' and word != 'if'\
+			and word != 'are' and word != 'when' and word != 'the' and \
+			word != 'and' and word != 'in' and word != 'my' and word.find('@') == -1\
+			and word != 'at' and word != 'on' and word != 'there' and \
+			word != 'their' and word != 'theyre' and word != 'how' and word != 'as' \
+			and word != 'if' and word != 'was' and word != 'has' and word != 'had' \
+			and word != 'is' and word != " " and word.find('=') == -1 and word.find('_') == -1 \
+			and word.find('<') == -1 and word.find('>') == -1:
 
-			# Unite filler and flashback counts since they tend to be correlated
-			if word.find('filler') != -1 or word.find('flashback') != -1 or word.find("recap") != -1:
-				fillerCount +=1
+				# Unite filler and flashback counts since they tend to be correlated
+				if word.find('filler') != -1 or word.find('flashback') != -1 or word.find("recap") != -1:
+					fillerCount +=1
 
-				if incrementFlag == False:
-					commentsWithFillerWords += 1
-					incrementFlag = True # so we won't increment anymore during this comment
+					if incrementFlag == False:
+						commentsWithFillerWords += 1
+						incrementFlag = True # so we won't increment anymore during this comment
 
-			# increment word count so we know how many words are in all the comments for the episode
-			wordCount += 1
+				# increment word count so we know how many words are in all the comments for the episode
+				wordCount += 1
 
 	print " word count: " + str(wordCount)
 	print " filler words count " + str(fillerCount)
